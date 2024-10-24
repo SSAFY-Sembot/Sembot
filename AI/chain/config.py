27b0d552759eba79b0c459fc26613066ab9e765b
김상혁 <ksang0728@gmail.com>
2024-10-24 14:43:12 +0900

    feat: save
    - 내용
    
    티켓: S11P31S102-32

---
+++ AI/chain/config.py
@@ -0,0 +1,9 @@
+# Paths
+PDF_PATH = r"../kisa_pdf/"
+VECTORSTORE_PATH = r"./vector_store_path"
+MODEL_PATH = r"llama3.1"
+
+# Embeddings Configuration
+EMBEDDINGS_MODEL_NAME = "jhgan/ko-sroberta-nli"
+EMBEDDINGS_MODEL_KWARGS = {"device": "cpu"}
+EMBEDDINGS_ENCODE_KWARGS = {"normalize_embeddings": True}
