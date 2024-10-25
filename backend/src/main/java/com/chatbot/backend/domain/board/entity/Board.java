27c472ad6abc6ae704c3df3080534d88e04c2fe8
변지환 <jihwaja9091@naver.com>
2024-10-25 17:37:11 +0900

    feat: 게시판 생성 기능 추가
    - 게시판 생성 기능을 추가했습니다
    티켓: S11P31S102-42

---
+++ backend/src/main/java/com/chatbot/backend/domain/board/entity/Board.java
@@ -0,0 +1,33 @@
+package com.chatbot.backend.domain.board.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board {
+
+    @Id
+    private Long id;
+
+    private String title;
+    private String content;
+    private int level;
+    private String category;
+    private String ruleURL;
+
+
+    @Builder
+    public Board(String title, String content, int level,String category ,String ruleURL ){
+        this.title = title;
+        this.content = content;
+        this.category = category;
+        this.level = level;
+        this.ruleURL = ruleURL;
+
+    }
+}
---
+++ backend/src/main/java/com/chatbot/backend/domain/board/exception/CreateBoardException.java
@@ -0,0 +1,4 @@
+package com.chatbot.backend.domain.board.exception;
+
+public class CreateBoardException {
+}
