46d0a9539821e634082a2d2768d65168b3b7280d
변지환 <jihwaja9091@naver.com>
2024-10-28 17:24:36 +0900

    Feat: add register logo

---
+++ Frontend/chatbot/src/pages/register/Register.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import { SignUpDTO } from './SignUp';
+
+interface SignUpFormProps{
+    onSubmit: (formData: SignUpDTO) => void;
+}
+
+const SignUpForm: React.FC<SignUpFormProps> = ({onSubmit}) =>{
+    const 
+}
+
+export default SignUpForm;
