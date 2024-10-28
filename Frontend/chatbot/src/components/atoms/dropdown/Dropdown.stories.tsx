da0ff46ad86a171e1308ec0c598d237bf3260ee0
이서빈 <bommy00@naver.com>
2024-10-28 16:44:28 +0900

    chore: 주석 제거
    - 내용
    Stories 불필요한 주석 제거
    
    티켓: S11P31S102-35

--- Frontend/chatbot/src/components/atoms/dropdown/Dropdown.stories.tsx
+++ Frontend/chatbot/src/components/atoms/dropdown/Dropdown.stories.tsx
@@ -2,15 +2,12 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import Dropdown from "./Dropdown";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "COMPONENTS/ATOMS/DROPDOWN/Dropdown",
   component: Dropdown,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
 } satisfies Meta<typeof Dropdown>;
 
