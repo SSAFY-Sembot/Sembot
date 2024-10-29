87621594d3582f725c7c561049d8de0e413ec84c
변지환 <jihwaja9091@naver.com>
2024-10-29 17:42:45 +0900

    Feat: entity setting

---
+++ backend/src/main/java/com/chatbot/backend/domain/chat/entitiy/Chat.java
@@ -0,0 +1,6 @@
+package com.chatbot.backend.domain.chat.entitiy;
+
+import jakarta.persistence.Entity;
+
+public class Chat {
+}
---
+++ backend/src/main/java/com/chatbot/backend/domain/user/entity/User.java
@@ -0,0 +1,14 @@
+package com.chatbot.backend.domain.user.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
