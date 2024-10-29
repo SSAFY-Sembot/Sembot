d6c136579573422aea0a16f2b8206646e11b4d82
KwangHyun <imleokkkk@naver.com>
2024-10-29 10:59:53 +0900

    feat: 관리자 페이지 사이드바 붙이기
    - 내용
     관리자 페이지 사이드바 붙이기
    
    티켓: S11P31S102-

--- frontend/chatbot/src/components/atoms/sidebar/Sidebar.stories.tsx
+++ frontend/chatbot/src/components/atoms/sidebar/Sidebar.stories.tsx
@@ -1,24 +1,24 @@
 import { Meta, Story } from "@storybook/react";
-import Siderbar from "./Siderbar"; // Adjust path if necessary
+import Sidebar from "./Sidebar"; // Adjust path if necessary
 
 // main page가 채팅방
 const componentsList = [
-  {
-    btnName: "새채팅",
-    styleName: "flex bg-blue-500 text-white py-2 px-4 rounded mx-1",
-    icon: "/src/assets/icons/plus.svg",
-  },
-  {
-    btnName: "채팅방1",
-    styleName: "flex bg-blue-500 text-white py-2 px-4 rounded mx-1",
-    icon: "/src/assets/icons/plus.svg",
-  },
-  {
-    btnName: "채팅방2",
-    styleName: "flex bg-blue-500 text-white py-2 px-4 rounded mx-1",
-    icon: "/src/assets/icons/plus.svg",
-  },
-  // 추가 컴포넌트 데이터들
+	{
+		btnName: "새채팅",
+		styleName: "flex bg-blue-500 text-white py-2 px-4 rounded mx-1",
+		icon: "/src/assets/icons/plus.svg",
+	},
+	{
+		btnName: "채팅방1",
+		styleName: "flex bg-blue-500 text-white py-2 px-4 rounded mx-1",
+		icon: "/src/assets/icons/plus.svg",
+	},
+	{
+		btnName: "채팅방2",
+		styleName: "flex bg-blue-500 text-white py-2 px-4 rounded mx-1",
+		icon: "/src/assets/icons/plus.svg",
+	},
+	// 추가 컴포넌트 데이터들
 ];
 
 // main page가 규정 목록일 때
@@ -42,32 +42,32 @@ const componentsList = [
 // ];
 
 const footerComponentsList = [
-  {
-    btnName: "footer Click Me 1",
-    styleName: "flex bg-blue-500 text-white py-2 px-4 rounded mx-1",
-    icon: "/src/assets/icons/plus.svg",
-  },
-  {
-    btnName: "footer Click Me 2",
-    styleName: "flex bg-blue-500 text-white py-2 px-4 rounded mx-1",
-    icon: "/src/assets/icons/plus.svg",
-  },
-  // 추가 컴포넌트 데이터들
+	{
+		btnName: "footer Click Me 1",
+		styleName: "flex bg-blue-500 text-white py-2 px-4 rounded mx-1",
+		icon: "/src/assets/icons/plus.svg",
+	},
+	{
+		btnName: "footer Click Me 2",
+		styleName: "flex bg-blue-500 text-white py-2 px-4 rounded mx-1",
+		icon: "/src/assets/icons/plus.svg",
+	},
+	// 추가 컴포넌트 데이터들
 ];
 
 // Meta information for Storybook
 export default {
-  title: "COMPONENTS/ATOMS/SIDEBAR/Siderbar",
-  component: Siderbar,
+	title: "COMPONENTS/ATOMS/SIDEBAR/Siderbar",
+	component: Sidebar,
 } as Meta;
 
 // Template for the story
 const Template: Story = () => (
-  <Siderbar
-    components={componentsList}
-    footerComponents={footerComponentsList}
-    isRule={false}
-  />
+	<Sidebar
+		components={componentsList}
+		footerComponents={footerComponentsList}
+		isRule={false}
+	/>
 );
 
 // Default story
