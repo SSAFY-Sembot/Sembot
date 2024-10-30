c2e8361b7bd9ac30ac7c4775692a793f6c21d38b
변지환 <jihwaja9091@naver.com>
2024-10-30 11:11:06 +0900

    Feat: add create chatroom api

--- backend/src/main/java/com/chatbot/backend/domain/chatroom/controller/ChatRoomController.java
+++ backend/src/main/java/com/chatbot/backend/domain/chatroom/controller/ChatRoomController.java
@@ -1,10 +1,28 @@
 package com.chatbot.backend.domain.chatroom.controller;
 
 
+import com.chatbot.backend.domain.chatroom.dto.request.CreateChatRoomRequestDto;
+import com.chatbot.backend.domain.chatroom.dto.response.CreateChatRoomResponseDto;
+import com.chatbot.backend.domain.chatroom.service.ChatRoomServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/chatrooms")
+@RequiredArgsConstructor
 public class ChatRoomController {
 
+    private final ChatRoomServiceImpl chatRoomService;
 
+    @PostMapping("")
+    public ResponseEntity<CreateChatRoomResponseDto> createChatRoom(
+           @RequestBody CreateChatRoomRequestDto createChatRoomRequestDto)
+    {
+        CreateChatRoomResponseDto response = chatRoomService.createChatRoom(createChatRoomRequestDto);
+        return ResponseEntity.ok().body(response);
+    }
 }
---
+++ backend/src/main/java/com/chatbot/backend/domain/chatroom/dto/response/FindChatRoomListResponseDto.java
@@ -0,0 +1,4 @@
+package com.chatbot.backend.domain.chatroom.dto.response;
+
+public class FindChatRoomListResponseDto {
+}
