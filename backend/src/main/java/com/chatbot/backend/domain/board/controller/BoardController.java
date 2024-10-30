7b40fb602b0c744a858366d2ae764a30fbc23089
이서빈 <bommy00@naver.com>
2024-10-30 15:01:11 +0900

    refactor: 게시판 controller 코드 구조 변경
    - 내용
    게시판 controller 코드 구조 변경
    
    티켓: S11P31S102-60

--- backend/src/main/java/com/chatbot/backend/domain/board/controller/BoardController.java
+++ backend/src/main/java/com/chatbot/backend/domain/board/controller/BoardController.java
@@ -1,26 +1,24 @@
 package com.chatbot.backend.domain.board.controller;
 
-
-import com.chatbot.backend.domain.board.dto.request.CreateBoardRequestDto;
-import com.chatbot.backend.domain.board.dto.response.CreateBoardResponseDto;
-import com.chatbot.backend.domain.board.service.BoardServiceImpl;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/")
+import com.chatbot.backend.domain.board.dto.request.CreateBoardRequest;
+import com.chatbot.backend.domain.board.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController("/api/board")
 @RequiredArgsConstructor
 public class BoardController {
+	private final BoardService boardService;
 
-    private final BoardServiceImpl boardService;
-
-    @PostMapping("/board")
-    ResponseEntity<?> createBoard(
-            CreateBoardRequestDto createBoardRequestDto){
-        boardService.createBoard(createBoardRequestDto);
-
-        return ResponseEntity.ok().build();
-    }
+	@PostMapping
+	ResponseEntity<Void> createBoard(Authentication authentication, CreateBoardRequest createBoardRequest) {
+		Long userId = (long)authentication.getPrincipal();
+		boardService.createBoard(createBoardRequest);
+		return ResponseEntity.ok().build();
+	}
 }
