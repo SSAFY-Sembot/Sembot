7694fdb54e98423b0ba9f885cbdccab11e87b281
이서빈 <bommy00@naver.com>
2024-10-30 14:34:18 +0900

    feat: CORS 설정 구현
    - 내용
    CORS(Cross-Origin Resource Sharing) 설정을 담당
    환경 변수를 통하여 개발 환경과 운영 환경의 origin을 각각 설정할 수 있습니다.
    
    티켓: S11P31S102-60

---
+++ backend/src/main/java/com/chatbot/backend/global/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.chatbot.backend.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+	@Value("${cors.origin.development}")
+	private String developmentOrigin;
+
+	@Value("${cors.origin.production}")
+	private String productionOrigin;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedMethods("OPTIONS", "HEAD", "GET", "POST", "PUT", "PATCH", "DELETE")
+			.allowCredentials(true)
+			.allowedOrigins(
+				developmentOrigin,
+				productionOrigin
+			)
+			.allowedHeaders("*")
+			.exposedHeaders("*");
+	}
+}
