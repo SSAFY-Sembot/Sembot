b1f3181520639806ebc23ebe23dea3bec3381eed
이서빈 <bommy00@naver.com>
2024-10-30 14:38:58 +0900

    feat: JWT 토큰을 검증하고 인증 정보를 설정 코드 구현
    - 내용
    JWT 토큰을 검증하고 인증 정보를 설정하는 필터입니다.
    요청 헤더에서 토큰을 추출하고 검증합니다.
    
    티켓: S11P31S102-60

---
+++ backend/src/main/java/com/chatbot/backend/global/jwt/JwtFilter.java
@@ -0,0 +1,42 @@
+package com.chatbot.backend.global.jwt;
+
+import static org.springframework.boot.web.servlet.filter.ApplicationContextHeaderFilter.*;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+	private final JwtProvider jwtProvider;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+		String token = resolveToken(request);
+		if (token != null) {
+			jwtProvider.validateToken(token, false);
+			Authentication authentication = this.jwtProvider.getAuthentication(token);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+		filterChain.doFilter(request, response);
+	}
+
+	private String resolveToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+			return bearerToken.substring(7);
+		}
+		return null;
+	}
+}
