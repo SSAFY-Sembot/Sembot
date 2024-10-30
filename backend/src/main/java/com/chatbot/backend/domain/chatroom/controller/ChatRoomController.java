205acefe3295375b96cd0f0c542769042946c177
변지환 <jihwaja9091@naver.com>
2024-10-30 16:04:13 +0900

    Feat: add chatroom for spring security

--- backend/src/main/java/com/chatbot/backend/domain/chatroom/controller/ChatRoomController.java
+++ backend/src/main/java/com/chatbot/backend/domain/chatroom/controller/ChatRoomController.java
@@ -1,5 +1,12 @@
 package com.chatbot.backend.domain.chatroom.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.chatbot.backend.domain.chatroom.dto.request.CreateChatRoomRequestDto;
 import com.chatbot.backend.domain.chatroom.dto.request.FindChatRoomListRequestDto;
@@ -7,10 +14,8 @@ import com.chatbot.backend.domain.chatroom.dto.response.CreateChatRoomResponseDt
 import com.chatbot.backend.domain.chatroom.dto.response.FindChatRoomListResponseDto;
 import com.chatbot.backend.domain.chatroom.service.ChatRoomServiceImpl;
 import com.chatbot.backend.global.security.CustomUserDetails;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/chatrooms")
@@ -21,9 +26,8 @@ public class ChatRoomController {
 
     @PostMapping("")
     public ResponseEntity<CreateChatRoomResponseDto> createChatRoom(
-            @AuthenticationPrincipal CustomUserDetails userDetails,
-           @RequestBody CreateChatRoomRequestDto createChatRoomRequestDto)
-    {
+        @AuthenticationPrincipal CustomUserDetails userDetails,
+        @RequestBody CreateChatRoomRequestDto createChatRoomRequestDto) {
         createChatRoomRequestDto.setUserId(userDetails.getId());
         CreateChatRoomResponseDto response = chatRoomService.createChatRoom(createChatRoomRequestDto);
         return ResponseEntity.ok().body(response);
