4f2f4dbfe90f1c0f01c866d35d951517fba50987
이서빈 <bommy00@naver.com>
2024-11-01 18:01:08 +0900

    feat: BoardService에 update 메서드 추가
    - 내용
    BoardService에 update 메서드 추가
    
    티켓: S11P31S102-81

--- backend/src/main/java/com/chatbot/backend/domain/board/service/BoardService.java
+++ backend/src/main/java/com/chatbot/backend/domain/board/service/BoardService.java
@@ -3,8 +3,12 @@ package com.chatbot.backend.domain.board.service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.chatbot.backend.domain.board.dto.request.BoardCreateRequest;
+import com.chatbot.backend.domain.board.dto.request.BoardUpdateRequest;
 import com.chatbot.backend.domain.board.dto.response.BoardDetailResponse;
 
 public interface BoardService {
-	BoardDetailResponse createBoard(Long userId, BoardCreateRequest BoardCreateRequest, MultipartFile file);
+	BoardDetailResponse createBoard(Long userId, BoardCreateRequest boardCreateRequest, MultipartFile file);
+
+	void updateBoard(Long userId, Long boardId, BoardUpdateRequest boardUpdateRequest,
+		MultipartFile file);
 }
