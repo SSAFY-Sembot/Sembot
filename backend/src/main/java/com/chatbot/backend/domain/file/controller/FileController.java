fe5f88573aac575cbc23d68dab2fdf0b71d15e0f
이서빈 <bommy00@naver.com>
2024-11-01 10:13:39 +0900

    feat: File 업로드 API 구현
    - 내용
    File 업로드 API 구현
    Multipart 파일을 받아 URL을 return 해주는 API를 구현하였습니다.
    
    티켓: S11P31S102-80

---
+++ backend/src/main/java/com/chatbot/backend/domain/file/controller/FileController.java
@@ -0,0 +1,36 @@
+package com.chatbot.backend.domain.file.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.chatbot.backend.domain.file.service.FileService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Tag(name = "File", description = "파일 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/files")
+@Slf4j
+public class FileController {
+	private final FileService fileService;
+
+	@Operation(
+		summary = "파일 URL 생성",
+		description = "새로운 파일 URL을 생성합니다. MultipartFile을 첨부할 수 있습니다."
+	)
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+	public ResponseEntity<String> createFile(
+		@RequestPart(value = "file", required = true) MultipartFile file
+	) {
+		return ResponseEntity.ok().body(fileService.saveFile(file));
+	}
+}
