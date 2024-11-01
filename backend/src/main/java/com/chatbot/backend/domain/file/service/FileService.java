7b26ba77688de48b10251ffc02af1f85ddd52b23
이서빈 <bommy00@naver.com>
2024-11-01 10:10:53 +0900

    refactor: File Controller 생성을 위하여 File 관련 코드 위치 수정
    - 내용
    File Controller 생성을 위하여 File 관련 코드 위치 수정
    Global에 위치한 File 관련 코드를 Domain으로 이동시켰습니다.
    
    티켓: S11P31S102-80

---
+++ backend/src/main/java/com/chatbot/backend/domain/file/service/FileService.java
@@ -0,0 +1,7 @@
+package com.chatbot.backend.domain.file.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileService {
+	String saveFile(MultipartFile file, String dirName);
+}
