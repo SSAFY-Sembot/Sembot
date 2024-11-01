a2cbb551b7055162e59923bb0053f6da98375867
한수한 <hshhan1009@naver.com>
2024-11-01 16:39:03 +0900

    chore : 라이브러리 설정 추가 및 기타 설정 변경

--- frontend/chatbot/vite.config.ts
+++ frontend/chatbot/vite.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '@configs': path.resolve(__dirname, 'src/configs'),
+      '@apis': path.resolve(__dirname, 'src/apis'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@pages': path.resolve(__dirname, 'src/pages/'),
       '@': path.resolve(__dirname, 'src'), // '@'를 'src'로 매핑
