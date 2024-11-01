23935ad3e3402ab274cc8c808845f2919e986b89
김상혁 <ksang0728@gmail.com>
2024-11-01 09:38:50 +0900

    feat: pdf loader 변경
    - pypdf 라이브러리 대신 pdf plumber로 교체했습니다.
    
    티켓: S11P31S102-74

--- AI/chain/vectorstore_manager.py
+++ AI/chain/vectorstore_manager.py
@@ -1,7 +1,40 @@
-from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import PyPDFDirectoryLoader
+import os
+
 from langchain.memory import ConversationBufferWindowMemory
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PDFPlumberLoader, PyPDFDirectoryLoader
+from langchain_community.vectorstores import FAISS
+
+
+# PDF 파일명들을 로드하는 함수
+def load_pdf_files(pdf_path):
+    """주어진 디렉토리에서 모든 PDF 파일들을 로드합니다."""
+    pdf_files = []
+    for root, dirs, files in os.walk(pdf_path):
+        for file in files:
+            if file.endswith(".pdf"):
+                pdf_files.append(os.path.join(root, file))
+    return pdf_files
+
+
+# PDF를 JSON으로 변환하는 함수
+def pdf_splitter(pdf_path):
+    pdf_files = load_pdf_files(pdf_path)
+    all_documents = []
+
+    for pdf_file in pdf_files:
+        loader = PDFPlumberLoader(pdf_file)
+        documents = loader.load()  # PDF 문서를 로드합니다
+
+        # 텍스트를 작은 청크로 분할
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=512,  # 각 청크의 최대 문자 수
+            chunk_overlap=64,  # 청크 간 중복되는 문자 수
+        )
+        chunked_documents = text_splitter.split_documents(documents)
+        all_documents.extend(chunked_documents)
+
+    return all_documents
 
 
 def load_vectorstore(vectorstore_path, embeddings):
@@ -12,19 +45,8 @@ def load_vectorstore(vectorstore_path, embeddings):
 
 
 def create_vectorstore(pdf_path, vectorstore_path, embeddings):
-    # 단계 1: 문서 로드
-    loader = PyPDFDirectoryLoader(pdf_path)
-    docs = loader.load()
 
-    # 단계 2: 문서 분할
-    recursive_text_splitter = RecursiveCharacterTextSplitter(
-        # 정말 작은 청크 크기를 설정합니다.
-        chunk_size=1000,
-        chunk_overlap=50,
-        length_function=len
-    )
-
-    chunked_docs = recursive_text_splitter.split_documents(docs)
+    chunked_docs = pdf_splitter(pdf_path)
 
     # 단계 3,4: 임베딩 & 벡터 스토어 생성
     # FAISS 사용
