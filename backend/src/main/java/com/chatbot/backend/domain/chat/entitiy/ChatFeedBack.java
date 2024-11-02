84bd1abb713a71fb5bd9dca9466243292d266f99
jihwan <jihwaja9091@naver.com>
2024-11-02 22:50:48 +0900

    Feat: add Feedback document

--- backend/src/main/java/com/chatbot/backend/domain/chat/entitiy/ChatFeedBack.java
+++ backend/src/main/java/com/chatbot/backend/domain/chat/entitiy/ChatFeedBack.java
@@ -1,4 +1,16 @@
 package com.chatbot.backend.domain.chat.entitiy;
 
+
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToOne;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "chatfeedbacks")
 public class ChatFeedBack {
+
+    private Long chatFeedBackId;
+
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Chat chat;
 }
