bf55c0e6995c5db378883a15a5149736d11337cf
변지환 <jihwaja9091@naver.com>
2024-11-04 10:20:53 +0900

    Feat: add userdetails service

--- backend/src/main/java/com/chatbot/backend/domain/chat/entitiy/ChatFeedBack.java
+++ backend/src/main/java/com/chatbot/backend/domain/chat/entitiy/ChatFeedBack.java
@@ -1,4 +1,14 @@
 package com.chatbot.backend.domain.chat.entitiy;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Document(collation = "chatfeedbacks")
 public class ChatFeedBack {
+
+	@Id
+	@GeneratedValue
+	
 }
