f860cd368fef60158f3be07fada3073ebfcdffd1
이서빈 <bommy00@naver.com>
2024-11-04 17:59:36 +0900

    refactor: Board Service로부터 검증 로직 분리
    - 내용
    Board Service로부터 검증 로직 분리
    
    티켓: S11P31S102-91

--- backend/src/main/java/com/chatbot/backend/domain/board/service/BoardServiceImpl.java
+++ backend/src/main/java/com/chatbot/backend/domain/board/service/BoardServiceImpl.java
@@ -9,17 +9,14 @@ import com.chatbot.backend.domain.board.dto.request.BoardUpdateRequest;
 import com.chatbot.backend.domain.board.dto.response.BoardDetailResponse;
 import com.chatbot.backend.domain.board.entity.Board;
 import com.chatbot.backend.domain.board.entity.BoardLike;
-import com.chatbot.backend.domain.board.exception.BoardAccessDeniedException;
-import com.chatbot.backend.domain.board.exception.BoardCreationNotAuthorizedException;
-import com.chatbot.backend.domain.board.exception.BoardUnauthorizedException;
 import com.chatbot.backend.domain.board.repository.BoardLikeRepository;
 import com.chatbot.backend.domain.board.repository.BoardRepository;
+import com.chatbot.backend.domain.board.util.BoardValidator;
 import com.chatbot.backend.domain.category.entity.Category;
 import com.chatbot.backend.domain.category.repository.CategoryRepository;
 import com.chatbot.backend.domain.file.service.FileService;
 import com.chatbot.backend.domain.user.entity.User;
 import com.chatbot.backend.domain.user.repository.UserRepository;
-import com.chatbot.backend.global.jwt.Role;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -36,19 +33,23 @@ public class BoardServiceImpl implements BoardService {
 	private final FileService fileService;
 	private final CategoryRepository categoryRepository;
 	private final BoardLikeRepository boardLikeRepository;
+	private final BoardValidator boardValidator;
 
 	@Override
 	public BoardDetailResponse createBoard(Long userId, BoardCreateRequest boardCreateRequest, MultipartFile file) {
-		// 검증
+		// 검증 & 조회
 		User user = userRepository.findByIdOrElseThrow(userId);
 		Category category = categoryRepository.findByNameOrElseThrow(boardCreateRequest.category());
-		validateUserWriteAuthorized(user);
+
+		boardValidator.validateUserWriteAuthorized(user);
 
 		// Board 생성 (비즈니스 로직)
-		Board board = boardCreateRequest.toEntity(user, category, fileService.saveFile(file, BOARD_UPLOAD_DIR));
-		Board savedBoard = boardRepository.save(board);
+		Board board = boardCreateRequest.toEntity(
+			user,
+			category,
+			fileService.saveFile(file, BOARD_UPLOAD_DIR));
 
-		return BoardDetailResponse.of(savedBoard);
+		return BoardDetailResponse.of(boardRepository.save(board));
 	}
 
 	@Override
@@ -58,51 +59,43 @@ public class BoardServiceImpl implements BoardService {
 		User user = userRepository.findByIdOrElseThrow(userId);
 		Category category = categoryRepository.findByNameOrElseThrow(boardUpdateRequest.category());
 		Board board = boardRepository.findByIdOrElseThrow(boardId);
-		BoardLike boardLike = boardLikeRepository.findByBoardIdAndUserId(boardId, userId).orElse(null);
-		validateUserAuthorizationForBoard(user, board);
+
+		boardValidator.validateBoardExists(board);
+		boardValidator.validateUserAuthorization(user, board);
 
 		// Board 수정 (비즈니스 로직)
-		board.updateBoard(boardUpdateRequest, category, fileService.saveFile(file, BOARD_UPLOAD_DIR));
+		board.updateBoard(
+			boardUpdateRequest,
+			category,
+			fileService.saveFile(file, BOARD_UPLOAD_DIR));
 
+		BoardLike boardLike = boardLikeRepository.findByBoardIdAndUserId(boardId, userId).orElse(null);
 		return BoardDetailResponse.of(board, boardLike);
 	}
 
 	@Override
 	public void deleteBoard(Long userId, Long boardId) {
+		// 검증 & 조회
 		User user = userRepository.findByIdOrElseThrow(userId);
 		Board board = boardRepository.findByIdOrElseThrow(boardId);
-		validateUserAuthorizationForBoard(user, board);
+
+		boardValidator.validateBoardExists(board);
+		boardValidator.validateUserAuthorization(user, board);
 
 		board.deleteBoard();
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public BoardDetailResponse getBoard(Long userId, Long boardId) {
+		// 검증 & 조회
 		User user = userRepository.findByIdOrElseThrow(userId);
 		Board board = boardRepository.findByIdOrElseThrow(boardId);
+
+		boardValidator.validateBoardExists(board);
+		boardValidator.validateBoardAccess(user, board);
+
 		BoardLike boardLike = boardLikeRepository.findByBoardIdAndUserId(boardId, userId).orElse(null);
-		validateUserHasAccess(user, board);
-
 		return BoardDetailResponse.of(board, boardLike);
-	}
-
-	// 사용자가 쓰기 권한이 있는 지 검증 (검증 로직)
-	private void validateUserWriteAuthorized(User user) {
-		if (user.getRole() != Role.USER_WRITE) {
-			throw new BoardCreationNotAuthorizedException();
-		}
-	}
-
-	// 사용자가 게시글을 수정 / 삭제할 권한이 있는 지 검증 (검증 로직)
-	private void validateUserAuthorizationForBoard(User user, Board board) {
-		if (user.getId() != board.getUser().getId()) {
-			throw new BoardUnauthorizedException();
-		}
-	}
-
-	private void validateUserHasAccess(User user, Board board) {
-		if (user.getLevel() < board.getLevel()) {
-			throw new BoardAccessDeniedException();
-		}
 	}
 }
