dafc20640a680f5a6fc0d598639a5f89efc71c08
이서빈 <bommy00@naver.com>
2024-11-04 18:00:23 +0900

    feat: Board Like 삭제 메서드 추가
    - 내용
    Board Like 삭제 메서드 추가
    
    티켓: S11P31S102-91

--- backend/src/main/java/com/chatbot/backend/domain/board/service/BoardLikeService.java
+++ backend/src/main/java/com/chatbot/backend/domain/board/service/BoardLikeService.java
@@ -2,4 +2,6 @@ package com.chatbot.backend.domain.board.service;
 
 public interface BoardLikeService {
 	void createBoardLike(Long userId, Long boardId);
+
+	void deleteBoardLike(Long userId, Long boardId);
 }
