85567f1ea689253cc886d0506162eb850ad7f677
이서빈 <bommy00@naver.com>
2024-11-05 17:45:46 +0900

    feat: Board 조회 API 구현
    - 내용
    Board 조회 API 구현
    
    티켓: S11P31S102-100

--- backend/src/main/java/com/chatbot/backend/domain/board/controller/BoardController.java
+++ backend/src/main/java/com/chatbot/backend/domain/board/controller/BoardController.java
@@ -1,11 +1,16 @@
 package com.chatbot.backend.domain.board.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,7 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.chatbot.backend.domain.board.dto.request.BoardCreateRequest;
+import com.chatbot.backend.domain.board.dto.request.BoardSearchCondition;
 import com.chatbot.backend.domain.board.dto.request.BoardUpdateRequest;
+import com.chatbot.backend.domain.board.dto.response.BoardBaseResponse;
 import com.chatbot.backend.domain.board.dto.response.BoardDetailResponse;
 import com.chatbot.backend.domain.board.service.BoardLikeService;
 import com.chatbot.backend.domain.board.service.BoardService;
@@ -114,5 +121,19 @@ public class BoardController {
 	) {
 		boardLikeService.deleteBoardLike(userDetails.getId(), boardId);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+
+	@Operation(
+		summary = "게시글 목록 조회",
+		description = "게시글 목록을 조회합니다."
+	)
+	@GetMapping
+	public ResponseEntity<Page<BoardBaseResponse>> getBoardList(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@ModelAttribute BoardSearchCondition boardSearchCondition,
+		@PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+	) {
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(boardService.getBoardList(userDetails.getId(), boardSearchCondition, pageable));
 	}
 }
