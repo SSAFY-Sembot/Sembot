8b0e605b613fd85373b62372b759040b6a580b1c
변지환 <jihwaja9091@naver.com>
2024-11-05 11:01:23 +0900

    "Feat: 채팅 메시지 mongoDB와 연결 확인"

--- backend/src/main/java/com/chatbot/backend/domain/chat/repository/MongoChatRepository.java
+++ backend/src/main/java/com/chatbot/backend/domain/chat/repository/MongoChatRepository.java
@@ -2,13 +2,14 @@ package com.chatbot.backend.domain.chat.repository;
 
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.chatbot.backend.domain.chat.entitiy.Chat;
 
 @Repository
-public interface MongoChatRepository extends MongoRepository<Chat, String> {
+public interface MongoChatRepository extends MongoRepository<Chat, ObjectId> {
 
 	List<Chat> findAllByChatRoom_IdOrderByCreatedAtDesc(Long chatRoomId);
 }
