35970ebc6d1512aa5c1d9fb7823946bae842deb4
KwangHyun <imleokkkk@naver.com>
2024-11-05 18:10:31 +0900

    feat:
    - 내용
    티켓: S11P31S102-

--- backend/src/main/java/com/chatbot/backend/domain/admin/service/AdminServiceImpl.java
+++ backend/src/main/java/com/chatbot/backend/domain/admin/service/AdminServiceImpl.java
@@ -1,17 +1,20 @@
 package com.chatbot.backend.domain.admin.service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.chatbot.backend.domain.admin.dto.response.ContentDto;
 import com.chatbot.backend.domain.admin.dto.response.FeedbackResponseDto;
 import com.chatbot.backend.domain.admin.exception.NoAuthorityException;
 import com.chatbot.backend.domain.category.entity.Category;
 import com.chatbot.backend.domain.category.exception.CategoryAlreadyExistsException;
 import com.chatbot.backend.domain.category.repository.CategoryRepository;
-import com.chatbot.backend.domain.chat.entity.Chat;
 import com.chatbot.backend.domain.chat.entity.ChatFeedBack;
 import com.chatbot.backend.domain.chat.repository.MongoChatFeedBackRepository;
 import com.chatbot.backend.domain.chat.repository.MongoChatRepository;
@@ -40,18 +43,11 @@ public class AdminServiceImpl implements AdminService {
 			throw new NoAuthorityException();
 		}
 
-		Page<ChatFeedBack> feedBacks = mongoChatFeedBackRepository.findAllOrderByCreatedAtByChatFeedBackIdDesc(pageable);
-		log.info("피드백 : {} {} {} {}", feedBacks.getSize(), feedBacks.getNumber(), feedBacks.getTotalPages(),
-			feedBacks.getTotalElements());
+		Page<ChatFeedBack> feedBacks = mongoChatFeedBackRepository.findAllByOrderByCreatedAtDesc(pageable);
+		List<ContentDto> contents = new ArrayList<>();
+		feedBacks.forEach(chatFeedBack -> contents.add(toContentDto(chatFeedBack)));
 
-		// FeedbackResponseDto.builder()
-		// 	.page(pageable.getPageNumber())
-		// 	.size(pageable.getPageSize())
-		// 	.build();
-		//
-		// feedBacks.stream()
-		// 	.map(chatFeedBack -> {})
-		return null;
+		return toFeedbackResponseDto(feedBacks, contents);
 	}
 
 	@Override
@@ -73,5 +69,27 @@ public class AdminServiceImpl implements AdminService {
 		categoryRepository.save(adminCategory);
 	}
 
+	public ContentDto toContentDto(ChatFeedBack chatFeedBack){
+		return
+		ContentDto.builder()
+			.chatId(String.valueOf(chatFeedBack.getChat().getChatId()))
+			.question(chatFeedBack.getChat().getQuestion())
+			.answer(chatFeedBack.getChat().getAnswer())
+			.isPositive(chatFeedBack.getChat().getIsPositive())
+			.negativeReason(chatFeedBack.getNegativeReason())
+			.createdAt(LocalDateTime.now())
+			.build();
+	}
+
+	public FeedbackResponseDto toFeedbackResponseDto(Page<ChatFeedBack> feedBacks, List<ContentDto> contents){
+		return
+		FeedbackResponseDto.builder()
+			.page(feedBacks.getNumber())
+			.size(feedBacks.getSize())
+			.totalPages(feedBacks.getTotalPages())
+			.totalElements(feedBacks.getTotalElements())
+			.contents(contents)
+			.build();
+	}
 
 }
