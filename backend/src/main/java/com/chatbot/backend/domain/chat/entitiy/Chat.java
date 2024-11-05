f6562f78c8d86ad2c28ef00790e31563b8822def
변지환 <jihwaja9091@naver.com>
2024-11-05 12:30:04 +0900

    Feat: 채팅 기능 구현확인

--- backend/src/main/java/com/chatbot/backend/domain/chat/entitiy/Chat.java
+++ backend/src/main/java/com/chatbot/backend/domain/chat/entitiy/Chat.java
@@ -3,12 +3,12 @@ package com.chatbot.backend.domain.chat.entitiy;
 import java.time.LocalDateTime;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.chatbot.backend.domain.chatroom.entity.ChatRoom;
 
 import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
