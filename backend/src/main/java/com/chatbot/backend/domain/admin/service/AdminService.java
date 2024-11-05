c2a00db8ccb6ba44efa40337a8cda156ee766fe7
KwangHyun <imleokkkk@naver.com>
2024-11-05 13:17:20 +0900

    feat:
    - 내용
    티켓: S11P31S102-

--- backend/src/main/java/com/chatbot/backend/domain/admin/service/AdminService.java
+++ backend/src/main/java/com/chatbot/backend/domain/admin/service/AdminService.java
@@ -1,5 +1,9 @@
 package com.chatbot.backend.domain.admin.service;
 
+import com.chatbot.backend.domain.admin.dto.request.FeedbackRequestDto;
+import com.chatbot.backend.domain.admin.dto.response.FeedbackResponseDto;
+
 public interface AdminService {
 	void createCategory(Long userId, String name);
+	FeedbackResponseDto findCategoryByPage(Long userId, FeedbackRequestDto request);
 }
