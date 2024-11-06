8fc5e3f6b4cdd405ed221544311495fced41aff6
한수한 <hshhan0221@naver.com>
2024-11-06 12:38:05 +0900

    feat: summarize api 추가

--- AI/sambot/app.py
+++ AI/sambot/app.py
@@ -13,7 +13,6 @@ BASE_DIR = os.getenv('BASE_DIR')
 VECTORSTORE_PATH = os.path.join(BASE_DIR, "vector_store_path")
 MODEL_PATH = os.path.join(BASE_DIR, "local_model")
 
-
 # --------------------- 검색기 만드는 부분 --------------------- #
 
 from langchain_huggingface import HuggingFaceEmbeddings
@@ -29,7 +28,6 @@ vectorstore = FAISS.load_local(
 
 # 검색기 생성
 retriever = vectorstore.as_retriever()
-
 
 # --------------------- 프롬프트 만드는 부분 --------------------- #
 
@@ -60,12 +58,11 @@ prompt = PromptTemplate.from_template(
 """
 )
 
-
 # --------------------- GPT LLM --------------------- #
 
 from langchain_openai import ChatOpenAI
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
 # --------------------- chain 만드는 부분 --------------------- #
 
@@ -80,13 +77,14 @@ def format_memory(memory):
         # qda에서 question 추출
         question = qda.get('question', '')
         if question:
-            conversation += f"User Before{i+1}: {question}\n"
+            conversation += f"User Before{i + 1}: {question}\n"
         # qda에서 answer 추출
         answer = qda.get('answer', '')
         if answer:
-            conversation += f"Assistant Before{i+1}: {answer}\n"
+            conversation += f"Assistant Before{i + 1}: {answer}\n"
         conversation += "\n"
     return conversation.strip()
+
 
 def format_docs(docs):
     context = ""
@@ -95,6 +93,7 @@ def format_docs(docs):
         source = doc.metadata.get('source', 'Unknown source')
         context += f"Document {i} (Source: {source}):\n{content}\n"
     return context.strip()
+
 
 def memory_retriever_func(inputs):
     memory = inputs["memory"]
@@ -105,46 +104,49 @@ def memory_retriever_func(inputs):
         question_text = qda.get('question', '')
         doc_content = qda.get('doc', {}).get('page_content', '')
         answer_text = qda.get('answer', '')
-        
+
         content = f"Question: {question_text}\n"
         if doc_content:
             content += f"Document Content: {doc_content}\n"
         content += f"Answer: {answer_text}"
         docs.append(Document(page_content=content))
-    
-    if not docs: # docs 없으면 없는거로 반환
+
+    if not docs:  # docs 없으면 없는거로 반환
         return []
 
-    memory_vectorstore = FAISS.from_documents(docs, embeddings) # 벡터 스토어 만들기
-    memory_retriever = memory_vectorstore.as_retriever() # 메모리 검색기 생성
-    retrieved_docs = memory_retriever.invoke(question) # 메모리 검색
+    memory_vectorstore = FAISS.from_documents(docs, embeddings)  # 벡터 스토어 만들기
+    memory_retriever = memory_vectorstore.as_retriever()  # 메모리 검색기 생성
+    retrieved_docs = memory_retriever.invoke(question)  # 메모리 검색
 
-    return retrieved_docs # 검색 결과 반환
+    return retrieved_docs  # 검색 결과 반환
+
 
 chain = (
-    RunnableMap(
-        {
-            "memory": RunnableLambda(lambda x: x["memory"]) | format_memory,
-            "memory_retriever": RunnableLambda(memory_retriever_func) | format_docs,
-            "retriever": RunnableLambda(lambda x: x["question"]) | retriever | format_docs,
-            "question": RunnableLambda(lambda x: x["question"]),
-        }
-    )
-    | prompt
-    | llm
-    | StrOutputParser()
+        RunnableMap(
+            {
+                "memory": RunnableLambda(lambda x: x["memory"]) | format_memory,
+                "memory_retriever": RunnableLambda(memory_retriever_func) | format_docs,
+                "retriever": RunnableLambda(lambda x: x["question"]) | retriever | format_docs,
+                "question": RunnableLambda(lambda x: x["question"]),
+            }
+        )
+        | prompt
+        | llm
+        | StrOutputParser()
 )
-
 
 # --------------------- FastAPI 설정 부분 --------------------- #
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Depends
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from pydantic import BaseModel
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Union
 import asyncio
+
+# pdf 요약기
+from summarize.summarizer import PDFSummarizer
 
 app = FastAPI()
 
@@ -158,12 +160,15 @@ app.add_middleware(
     expose_headers=["X-Request-ID"],  # 요청 ID를 노출하도록 설정
 )
 
+
 class SearchRequest(BaseModel):
     question: str
+
 
 class QueryRequest(BaseModel):
     memory: List[Dict[str, Any]]
     question: str
+
 
 async def async_stream_response(input_data: dict):
     # invoke 출력
@@ -191,6 +196,15 @@ async def stream_answer(query: QueryRequest):
     }
 
     return StreamingResponse(async_stream_response(input_data), media_type="text/plain")
+
+
+summarizer = PDFSummarizer(llm)
+
+@app.post("/summarize")
+async def summarize(file: Union[UploadFile, str] = File(...)):
+    result = await summarizer.summarize(file, use_parallel=True)
+    print(result["summary"])
+    return result["summary"]
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
