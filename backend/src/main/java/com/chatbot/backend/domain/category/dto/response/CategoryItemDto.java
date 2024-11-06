c13d0b5996f15c783506b440ac3e7b094f5a19e1
이서빈 <bommy00@naver.com>
2024-11-06 10:45:38 +0900

    feat: CategoryItemDto 생성 of 메서드 구현
    - 내용
    CategoryItemDto 생성 of 메서드 구현
    서비스 단의 로직 감소
    
    티켓: S11P31S102-101

--- backend/src/main/java/com/chatbot/backend/domain/category/dto/response/CategoryItemDto.java
+++ backend/src/main/java/com/chatbot/backend/domain/category/dto/response/CategoryItemDto.java
@@ -1,13 +1,24 @@
 package com.chatbot.backend.domain.category.dto.response;
 
+import com.chatbot.backend.domain.category.entity.Category;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @ToString
+@Builder
 public class CategoryItemDto {
 	private Long categoryId;
 	private String name;
+
+	public static CategoryItemDto of(Category category) {
+		return CategoryItemDto.builder()
+			.categoryId(category.getId())
+			.name(category.getName())
+			.build();
+	}
 }
