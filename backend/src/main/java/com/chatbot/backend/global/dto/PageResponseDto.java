d7a5d32241c7d4b0f473b34e101ad2253a504909
이서빈 <bommy00@naver.com>
2024-11-08 18:41:31 +0900

    feat: Page Response Dto 제네릭으로 변경

--- backend/src/main/java/com/chatbot/backend/global/dto/PageResponseDto.java
+++ backend/src/main/java/com/chatbot/backend/global/dto/PageResponseDto.java
@@ -1,22 +1,23 @@
-package com.chatbot.backend.domain.admin.dto.response;
+package com.chatbot.backend.global.dto;
 
-import lombok.Builder;
-import lombok.Getter;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 
+import lombok.Builder;
+import lombok.Getter;
+
 @Getter
 @Builder
-public class PageResponseDto {
-	private List<FeedbackResponseDto> contents;
+public class PageResponseDto<T> {
+	private List<T> contents;
 	private Integer page;
 	private Integer size;
 	private Integer totalPages;
 	private Long totalElements;
 
-	public static PageResponseDto of(Page<FeedbackResponseDto> pageInfo){
-		return PageResponseDto.builder()
+	public static <T> PageResponseDto<T> of(Page<T> pageInfo) {
+		return PageResponseDto.<T>builder()
 			.contents(pageInfo.getContent())
 			.page(pageInfo.getPageable().getPageNumber())
 			.size(pageInfo.getPageable().getPageSize())
