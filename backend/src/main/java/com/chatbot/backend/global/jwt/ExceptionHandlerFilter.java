8a4cf214fc67f3744f8cc8ab490aac864d1d4385
한수한 <hshhan0221@naver.com>
2024-11-11 20:21:04 +0900

    fix: filter exception handler에 cors 설정 추가

--- backend/src/main/java/com/chatbot/backend/global/jwt/ExceptionHandlerFilter.java
+++ backend/src/main/java/com/chatbot/backend/global/jwt/ExceptionHandlerFilter.java
@@ -2,6 +2,7 @@ package com.chatbot.backend.global.jwt;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -20,6 +21,8 @@ import jakarta.servlet.http.HttpServletResponse;
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
     
     private final ObjectMapper objectMapper = new ObjectMapper();
+    @Value("${cors.origin.development}")
+    private String developmentOrigin;
     
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -28,6 +31,13 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
         try {
             filterChain.doFilter(request, response);
         } catch (ServiceException e) {
+            // CORS 헤더 추가
+            response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods", "*");
+            response.setHeader("Access-Control-Allow-Headers", "*");
+            response.setHeader("Access-Control-Max-Age", "3600");
+
             setErrorResponse(response, e);
         }
     }
