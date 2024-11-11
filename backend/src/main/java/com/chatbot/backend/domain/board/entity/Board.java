80f38bda8728bdd9dc72541778eaf384ab8fe49b
이서빈 <bommy00@naver.com>
2024-11-11 18:13:31 +0900

    refactor: 수정 시에도 null 값 허용

--- backend/src/main/java/com/chatbot/backend/domain/board/entity/Board.java
+++ backend/src/main/java/com/chatbot/backend/domain/board/entity/Board.java
@@ -75,10 +75,10 @@ public class Board extends BaseTimeEntity {
 
 	// Board 정보 수정
 	public void updateBoard(BoardUpdateRequestDto boardUpdateRequestDto, Category category, String fileUrl) {
-		this.title = boardUpdateRequestDto.title();
+		this.title = boardUpdateRequestDto.title() == null ? this.title : boardUpdateRequestDto.title();
 		this.contents = boardUpdateRequestDto.contents() == null ? this.contents : boardUpdateRequestDto.contents();
-		this.level = boardUpdateRequestDto.level();
-		this.category = category;
+		this.level = boardUpdateRequestDto.level() == null ? this.level : boardUpdateRequestDto.level();
+		this.category = category == null ? this.category : category;
 		this.fileUrl = fileUrl == null ? this.fileUrl : fileUrl;
 	}
 
