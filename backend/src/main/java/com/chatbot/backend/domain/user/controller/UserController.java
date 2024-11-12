a606fd2c9d04fff73cb019821e5223acd6faa2b5
변지환 <jihwaja9091@naver.com>
2024-11-12 13:30:21 +0900

    Feat: 이메일중복확인 오류 수정

--- backend/src/main/java/com/chatbot/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/chatbot/backend/domain/user/controller/UserController.java
@@ -13,10 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.chatbot.backend.domain.user.dto.request.LoginRequestDto;
 import com.chatbot.backend.domain.user.dto.request.SignupRequestDto;
+import com.chatbot.backend.domain.user.exception.EmptyEmailException;
 import com.chatbot.backend.domain.user.service.UserService;
-import com.chatbot.backend.global.jwt.JwtProvider;
 import com.chatbot.backend.global.jwt.Role;
-import com.chatbot.backend.global.security.CustomUserDetails;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -33,33 +32,39 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping("/register")
-	public ResponseEntity<Void> signUp(@Valid @RequestBody SignupRequestDto signupRequestDto){
+	public ResponseEntity<Void> signUp(@Valid @RequestBody SignupRequestDto signupRequestDto) {
 		userService.signUp(signupRequestDto);
 		return ResponseEntity.ok().build();
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<Map<String,String>> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response){
+	public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequestDto loginRequestDto,
+		HttpServletResponse response) {
 		Role role = userService.login(loginRequestDto, response);
 		Map<String, String> result = new HashMap<>();
-		result.put("token",response.getHeader("Authorization"));
+		result.put("token", response.getHeader("Authorization"));
 		result.put("role", role.getRole());
 		return ResponseEntity.ok().body(result);
 	}
 
 	@PostMapping("/logout")
-	public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response){
-		userService.logout( request, response);
+	public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response) {
+		userService.logout(request, response);
 		return ResponseEntity.ok().build();
 	}
 
 	@GetMapping
-	public ResponseEntity<Boolean> isDuplicate(@RequestParam("email") String email){
+	public ResponseEntity<Boolean> isDuplicate(@RequestParam("email") String email) {
+
+		if (email == null) {
+			throw new EmptyEmailException();
+		}
+
 		return ResponseEntity.ok().body(userService.isDuplicate(email));
 	}
 
 	@PostMapping("/reissue")
-	public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response){
+	public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
 		userService.reissue(request, response);
 		return ResponseEntity.ok().build();
 	}
