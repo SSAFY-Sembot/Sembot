873998e479a8a2b6d2b6d19a5384d13b64e6f077
이서빈 <bommy00@naver.com>
2024-11-13 18:29:36 +0900

    refactor: Login 시 반환 값 수정

--- backend/src/main/java/com/chatbot/backend/domain/user/service/UserService.java
+++ backend/src/main/java/com/chatbot/backend/domain/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.chatbot.backend.domain.user.service;
 
 import com.chatbot.backend.domain.user.dto.request.LoginRequestDto;
 import com.chatbot.backend.domain.user.dto.request.SignupRequestDto;
+import com.chatbot.backend.domain.user.dto.response.LoginResponseDto;
 import com.chatbot.backend.global.jwt.Role;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public interface UserService {
 	void signUp(SignupRequestDto signupRequestDto);
-	Role login(LoginRequestDto loginRequestDto, HttpServletResponse response);
+	LoginResponseDto login(LoginRequestDto loginRequestDto, HttpServletResponse response);
 	void logout(HttpServletRequest request, HttpServletResponse response);
 	boolean isDuplicate(String email);
 	void reissue(HttpServletRequest request, HttpServletResponse response);
