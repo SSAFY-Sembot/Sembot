c948fde13eea78a397eaf2be727cccda16b7c47f
한수한 <hshhan0221@naver.com>
2024-11-18 17:00:04 +0900

    chore: config 설정 변경

--- frontend/chatbot/src/configs/config.ts
+++ frontend/chatbot/src/configs/config.ts
@@ -1,6 +1,5 @@
 // 환경 변수의 타입을 명시합니다.
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 const AI_URL = import.meta.env.VITE_AI_URL;
-const REDIRECT_URL = import.meta.env.VITE_REDIRECT_URL;
 
-export { BACKEND_URL, AI_URL, REDIRECT_URL };
+export { BACKEND_URL, AI_URL };
